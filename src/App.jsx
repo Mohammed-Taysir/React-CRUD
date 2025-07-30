abcb64bb3db860f096afcbd5fc6079be5f21a5f2
Mohammed Taysir Alkhatib <medo-11223344@hotmail.com>
2025-07-30 18:42:25 +0300

    first commet

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { Route, Routes } from "react-router-dom"
+import Footer from "./components/footer/Footer"
+import Navbar from "./components/navbar/Navbar"
+import Home from "./pages/home/Home"
+import Create from "./pages/user/Create"
+import About from "./pages/about/About"
+
+function App() {
+  return (
+    <>
+      <Navbar />
+        <Routes>
+          <Route path = "/" element = {<Home />} />
+          <Route path = "/users" element = {<Home />} />
+          <Route path = "/add" element = {<Create />} />
+          <Route path = "/about" element = {<About />} />
+          
+        </Routes>
+
+      <Footer />
+    </>
+  )
+}
+
+export default App
